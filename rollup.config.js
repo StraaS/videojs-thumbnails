d4c30042cd97b514f06d7903489d14e7fcfc875f
kiwi.liu <kiwi.liu@ikala.tv>
2019-01-18 10:00:43 +0800

    chore: configure rollup-plugin-serve & rollup-plugin-livereload

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
 const generateRollupConfig = require('videojs-generate-rollup-config')
 const { eslint } = require('rollup-plugin-eslint')
+const serve = require('rollup-plugin-serve')
+const livereload = require('rollup-plugin-livereload')
 
 const config = generateRollupConfig({
   input: 'src/videojs.thumbnails.js',
   checkWatch: true,
   plugins(defaultPlugins) {
-    return {
-      browser: ['eslint'].concat(defaultPlugins.browser),
-      module: ['eslint'].concat(defaultPlugins.module),
-      test: ['eslint'].concat(defaultPlugins.test),
-    }
+    const writeKeys = ['browser', 'module', 'test']
+
+    const newPlugins = {}
+    writeKeys.forEach((key) => {
+      newPlugins[key] = defaultPlugins[key]
+      newPlugins[key] = ['eslint'].concat(newPlugins[key])
+      newPlugins[key] = newPlugins[key].concat(['serve', 'livereload'])
+    })
+
+    return newPlugins
   },
   primedPlugins(defaults) {
     return Object.assign(defaults, {
@@ -17,6 +24,17 @@ const config = generateRollupConfig({
         useEslintrc: true,
         throwOnError: true,
         exclude: ['dist/**', 'node_modules/**'],
+      }),
+      serve: serve({
+        open: true,
+        openPage: '/demo/example.html',
+        contentBase: './',
+        host: 'localhost',
+        port: '10000',
+      }),
+      livereload: livereload({
+        watch: 'dist',
+        verbose: true,
       }),
     })
   },
